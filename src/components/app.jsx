f90db8d3e3aa3f46df817ac9a783eaf02b8a82d7
rajel89 <rajah.ello@yahoo.com>
2021-07-21 22:15:00 -0500

    created the initial structure of the titleBad.jsx file.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React,{ Component } from 'react';
 
 class App extends Component {
-    state ={}
+    state = {}
     render(){
         return(
             <h1>Our React App Using a Component</h1>
-        );
+        )
     }
 }
 
