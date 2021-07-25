798b32c9cb94bbe1cb89bcc6e7e78c74fe96c222
rajel89 <rajah.ello@yahoo.com>
2021-07-24 23:34:02 -0500

    completed page 22 of the react tutorial sheet with no problem.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,14 +2,36 @@ import React, { Component } from "react";
 import TitleBar from "./TitleBar/titleBar";
 
 class App extends Component {
-  state = {
-    render() {
-      return (
-        <div className="container-fluid">
-          <TitleBar />
+  constructor(props) {
+    super(props);
+    this.books = [
+      { title: "Ready Player One", author: "Ernest Cline" },
+      { title: "All the Light We Cannot See", author: "Anthony Doerr" },
+      { title: "The First and Last Freedom", author: "Jiddu Krishnamurti" },
+    ];
+    this.state = {
+      bookNumber: 1,
+    };
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <TitleBar />
+        <div className="row row-spacer">
+          <div className="col-md-4">
+            {/*Button here to move to the previous boook viewed*/}
+          </div>
+          <div className="col-md-4">
+              {/*Display Book with Cover here*/}
+              <h1>{this.books[this.state.bookNumber].title}</h1>
+              <h4>{this.books[this.state.bookNumber].author}</h4>
+          </div>
+          <div className="col-md-4">
+            {/*Button here to move to the previous book viewed*/}
+          </div>
         </div>
-      );
-    },
-  };
+      </div>
+    );
+  }
 }
 export default App;
